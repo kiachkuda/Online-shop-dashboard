d52d457c84e9bc86e140b3eabb421c1a8b30ec2f
unknown <kiachkuda@gmail.com>
2025-11-28 03:04:21 +0300

    Error Collection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 "use client"
 import Products from '@/app/ui/components/Products';
 import HeroCarousel from '@/app/ui/components/HeroCarousel';
+import { Suspense } from 'react';
 
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-0 sm:p-6 md:p-6 w-full bg-white">
       <HeroCarousel />
-      <Products />
+      <Suspense>
+        <Products />
+      </Suspense>
+      
     </main>
   );
 }
